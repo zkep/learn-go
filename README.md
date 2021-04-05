97b98bc94bd09ec698ee22ab543e5252e2852804
zhang.ka <773683464@qq.com>
2021-04-05 22:18:43 +0800

    更新装饰器模式

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ go日常学习，包括设计模式，高级的用法
 1. [单列模式](singleton.go)
 2. [工厂模式](factory.go)
 3. [创造者模式](builder.go)
-4. [原型模式](prototype.gp)
-5. [代理模式](proxy.gp)
-6. [桥接模式](bridge.gp)
+4. [原型模式](prototype.go)
+5. [代理模式](proxy.go)
+6. [桥接模式](bridge.go)
+6. [装饰器模式](decorator.go)
 
---
+++ design/decorator_test.go
@@ -0,0 +1,15 @@
+package design
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestColorSquare_Draw(t *testing.T) {
+	sq := Square{}
+	csq := NewColorSquare(sq, "red")
+	got := csq.Draw()
+	assert.Equal(t, "this is a square, color is red", got)
+}
