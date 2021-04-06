947f0aced882c62023214fefc5f43b555ba8890b
zhang.ka <773683464@qq.com>
2021-04-06 10:32:52 +0800

    更新适配器模式

---
+++ design/adapter_test.go
@@ -0,0 +1,21 @@
+package design
+
+
+import (
+	"testing"
+)
+
+func TestAliyunClientAdapter_CreateServer(t *testing.T) {
+	var a ICreateServer = &AliYunClientAdapter{
+		Client: AliYunClient{},
+	}
+	a.CreateServer(1.0, 2.0)
+}
+
+func TestQiNiuClientAdapter_CreateServer(t *testing.T) {
+	var a ICreateServer = &QiNiuClientAdapter{
+		Client: QiNiuClient{},
+	}
+
+	a.CreateServer(1.0, 2.0)
+}
