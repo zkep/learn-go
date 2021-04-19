f44ae2ee8e9b60fd1a9bc15c8a75b40eefc1f26b
zhang.ka <773683464@qq.com>
2021-04-19 12:22:21 +0800

    update observer

--- design/observer.go
+++ design/observer.go
@@ -1,6 +1,10 @@
 package design
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"sync"
+)
 
 type IObserver interface {
 	Update(msg string)
@@ -38,4 +42,59 @@ type ObserverTwo struct{}
 
 func (obj *ObserverTwo) Update(msg string) {
 	fmt.Printf("ObserverTwo:%s", msg)
+}
+
+type Bus interface {
+	Subscribe(topic string, handelr interface{}) error
+	Publish(topic string, args ...interface{})
+}
+
+type AsyncEventBus struct {
+	handlers map[string][]reflect.Value
+	lock     sync.Mutex
+}
+
+func NewAsyncEventBus() *AsyncEventBus {
+	return &AsyncEventBus{
+		handlers: map[string][]reflect.Value{},
+		lock:     sync.Mutex{},
+	}
+}
+
+func (self *AsyncEventBus) Subscribe(topic string, f interface{}) error {
+	self.lock.Lock()
+	defer self.lock.Unlock()
+
+	v := reflect.ValueOf(f)
+	if v.Type().Kind() != reflect.Func {
+		return fmt.Errorf("handler is not a function")
+	}
+
+	handler, ok := self.handlers[topic]
+	if !ok {
+		handler = []reflect.Value{}
+	}
+	handler = append(handler, v)
+	self.handlers[topic] = handler
+
+	return nil
+}
+
+func (self *AsyncEventBus) Publish(topic string, args ...interface{}) {
+	handlers, ok := self.handlers[topic]
+	if !ok {
+		fmt.Println("not found handlers in topic:", topic)
+		return
+	}
+	params := make([]reflect.Value, len(args))
+
+	for i, arg := range args {
+		params[i] = reflect.ValueOf(arg)
+	}
+
+	for i := range handlers {
+		go handlers[i].Call(params)
+	}
+
+	return
 }
--- design/observer_test.go
+++ design/observer_test.go
@@ -1,10 +1,32 @@
 package design
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func TestSubject_Notify(t *testing.T) {
 	sub := &Subject{}
 	sub.Register(&ObserverOne{})
 	sub.Register(&ObserverTwo{})
 	sub.Notify("hi")
+}
+
+func TestAsyncEventBus(t *testing.T) {
+	bus := NewAsyncEventBus()
+	bus.Subscribe("topic:1", subOne)
+	bus.Subscribe("topic:2", subTwo)
+	bus.Publish("topic:1", "test1", "test2")
+	bus.Publish("topic:1", "testA", "testB")
+	time.Sleep(1 * time.Second)
+}
+
+func subOne(msgs ...string) {
+	time.Sleep(1 * time.Microsecond)
+	fmt.Printf("subOne,%+v", msgs)
+}
+
+func subTwo(msgs ...string) {
+	fmt.Printf("subTwo,%+v", msgs)
 }
